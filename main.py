67c691448d215b300d4afae4a1309c153926c031
jelliebyte <147528422+jelliebyte@users.noreply.github.com>
2023-12-04 16:56:12 +0000

    can remove numbers :D

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import random
 game_run = True
 cardDeck = []
 cardDeck2 = []
-colorDeck = ["Red", "Green", "Yellow", "Blue"]
-getPlayer = random.randint(1, 2)
+colorDeck = ["Red", "Green", "Yellow", "Blue"] #0-3
+# getPlayer = random.randint(1, 2)
+getPlayer = 1
 randCard = random.randint(1, 3)
 
 for card in range(1, 9):
@@ -14,6 +15,11 @@ for card in range(1, 9):
   cardDeck2.append(random.randrange(1, 9) + randCard)
   cardDeck2.append(random.sample(colorDeck, k=1))
 
+def playCard():
+  choice = int(input("What would you like to play? "))
+  if choice in cardDeck:
+    cardDeck.remove(choice)
+  print(cardDeck)
 
 def playerTurn():
   global getPlayer
@@ -21,6 +27,7 @@ def playerTurn():
   if getPlayer == 1:
     print("player 1")
     print(cardDeck)
+    playCard()
     getPlayer = 2
   else:
     print("player 2")
