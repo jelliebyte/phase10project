ceb5eddc3c6d3adddc5bf10194979f74c04e82f3
jelliebyte <147528422+jelliebyte@users.noreply.github.com>
2023-12-04 05:01:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+
+game_run = True
+cardDeck = []
+cardDeck2 = []
+colorDeck = ["Red", "Green", "Yellow", "Blue"]
+getPlayer = random.randint(1, 2)
+randCard = random.randint(1, 3)
+
+for card in range(1, 9):
+  cardDeck.append(random.randrange(1, 9) + randCard)
+  cardDeck.append(random.sample(colorDeck, k=1))
+  cardDeck2.append(random.randrange(1, 9) + randCard)
+  cardDeck2.append(random.sample(colorDeck, k=1))
+
+
+def playerTurn():
+  global getPlayer
+  global game_run
+  if getPlayer == 1:
+    print("player 1")
+    print(cardDeck)
+    getPlayer = 2
+  else:
+    print("player 2")
+    print(cardDeck2)
+    game_run = False
+
+
+while game_run:
+  playerTurn()
